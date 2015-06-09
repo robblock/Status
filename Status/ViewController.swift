008effb3db7a698319806abf62da3ea9fda3e9a8
bobdawgg <RobBlock@Robert-Blocks-MacBook-Pro-2.local>
2015-06-08 19:28:24 -0700

    Add tableView with Data Source

--- Status/ViewController.swift
+++ Status/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - UITableViewDataSource
+    
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            return 0
+            
+            
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        //TODO: Make this cell re
+        var cell = UITableViewCell()
+        
+        return cell
+    }
 
 }
 

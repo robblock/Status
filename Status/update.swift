fea80508042010266bed3e918011151b2863f8da
bobdawgg <RobBlock@Robert-Blocks-MacBook-Pro-2.local>
2015-06-08 19:13:17 -0700

    Add Models

---
+++ Status/update.swift
@@ -0,0 +1,21 @@
+//
+//  update.swift
+//  Status
+//
+//  Created by Rob Block on 6/8/15.
+//  Copyright (c) 2015 Robert. All rights reserved.
+//
+
+import Foundation
+
+class Update {
+    
+    //each update has a user of type user
+    var user: User?
+    
+    var text: String?
+    
+    //dates are always NSDate
+    var date: NSDate?
+    
+}
---
+++ Status/user.swift
@@ -0,0 +1,17 @@
+//
+//  user.swift
+//  Status
+//
+//  Created by Rob Block on 6/8/15.
+//  Copyright (c) 2015 Robert. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var name: String?
+    var bio: String?
+    var username: String?
+    var link: String?
+    var city: String
+}
